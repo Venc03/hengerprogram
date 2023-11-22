d8a43e072fa2fc704c369fee8e6ce55b643ddb9c
Végi Dániel Márk (SZF_2022_2024) <vegi.daniel.mark@diak.szamalk-szalezi.hu>
2023-11-22 13:31:42 +0100

    henger program alap uml alapjan

---
+++ src/hengerprogram/Cso.java
@@ -0,0 +1,32 @@
+package hengerprogram;
+
+public class Cso extends TomorHenger{
+    
+    public Cso(double r, double m, double falvastagasag) {
+        super(r, m);
+    }
+    
+    public Cso(double r, double m, double fajsuly, double falvastagasag) {
+        super(r, m, fajsuly);
+    }
+    
+
+    public double getFalvastagsag(){
+        
+        return 0;
+        
+    }
+    
+    @Override
+    public double terfogat(){
+        
+        return 0;
+        
+    }
+
+    @Override
+    public String toString() {
+        return "Cso{" + '}';
+    }
+
+}
---
+++ src/hengerprogram/TomorHenger.java
@@ -0,0 +1,31 @@
+package hengerprogram;
+
+public class TomorHenger extends Henger {
+    
+    public TomorHenger(double r, double m, double fajsuly) {
+        super(r, m);
+    }
+    
+    public TomorHenger(double r, double m) {
+        super(r, m);
+    }
+    
+    public double getFajsuly(){
+        
+        return 0;
+        
+    }
+    
+    public double suly(){
+        
+        return 0;
+        
+    }
+
+    @Override
+    public String toString() {
+        return "TomorHenger{" + '}';
+    }
+    
+    
+}
